5fc2e482b3cd160ab262b5179b214ca83c19dfd2
Noel Diaz Rodriguez <noeldiazro@gmail.com>
2014-07-31 10:00:14 +0000

    Setup añadido

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+setup(name='piDA',
+      version='1.0',
+      description='Data acquisition management software',
+      author='Noel Diaz',
+      author_email='noeldiazro@gmail.com',
+      url='http://github.com/noeldiazro/piDA',
+      packages=['piDA','piDA.interfaces'],
+      ext_modules=[
+        Extension('clock', ['piDA/src/pyclock.c', 'piDA/src/tsop.c'], libraries=['rt']),
+        Extension('spidev', ['piDA/src/spidev_module.c'])
+      ],
+      data_files=[
+        ('/etc/modprobe.d',['piDA/config/raspi-blacklist.conf']),
+        ('/etc',['piDA/config/modules'])
+        ]
+)
