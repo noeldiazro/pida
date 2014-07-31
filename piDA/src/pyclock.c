06b676c77716e02aa33c26ed83834c2bc376196c
Noel Diaz Rodriguez <noeldiazro@gmail.com>
2014-07-31 11:42:07 +0000

    Mejora ejecución concurrente extensión clock

--- piDA/src/pyclock.c
+++ piDA/src/pyclock.c
@@ -5,8 +5,10 @@
 /* Returns a timespec with the current value of CLOCK_MONOTONIC */
 static PyObject *py_time(PyObject *self, PyObject *args) {
   struct timespec result;
-
+  
+  Py_BEGIN_ALLOW_THREADS
   clock_gettime(CLOCK_MONOTONIC, &result);
+  Py_END_ALLOW_THREADS
 
   return Py_BuildValue("d", ts_to_s(result));
 } 
@@ -19,9 +21,13 @@ static PyObject *py_sleep(PyObject *self, PyObject *args) {
   if (!PyArg_ParseTuple(args, "d", &request_d)) {
     return NULL;
   }
+
+  Py_BEGIN_ALLOW_THREADS
   request = double_to_ts(request_d);
   
   result = clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &request, NULL);
+  Py_END_ALLOW_THREADS
+
   return Py_BuildValue("i", result);
 }
 
